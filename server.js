d4ba01019e64b0ef14ecb1601150f9f221ccacf2
Juan Jose Pabon Tritton <juan.pabon@noviscorp.com>
2021-02-24 15:46:24 -0300

    config routes for the function

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 
 
+const testRouter = require('./routes/test')
+app.use('/test', testRouter)
+
 app.listen(3000, () => console.log('Server start'))
