5512260d1f909adcc50dcb54d6a55acf7095863d
Juan Jose Pabon Tritton <juan.pabon@noviscorp.com>
2021-02-24 17:15:09 -0300

    create validation for the value and fill in an array, for next be comparated

--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/:id', (req, res) =>{
-    res.send('Parameter ' + req.params.id)
+router.get('/:id', validateValue, fillArrayValues, (req, res) =>{
+    res.send('data set on array');
+    console.log(res.valueArray);
 })
+
+function validateValue(req, res, next){
+   (req.params.id > 1) ? next() : res.status(400).json({ message: 'the value is not valid' });
+}
+
+function fillArrayValues(req, res, next){
+    if(!res.vector){
+        res.vector = [];
+    }
+
+    res.vector.push(parseInt(req.params.id));
+
+    (req.params.id < 2) ? (res.valueArray = res.vector,  next()) : (req.params.id--, fillArrayValues(req, res, next));
+}
 
 module.exports = router;
